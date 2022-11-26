4dc0e31b845f3ace41e26a017ec2c373464651e5
Vi Ngoc Hoang <hoangvn@devcamp.edu.vn>
2022-11-26 19:47:00 +0700

    rm old

---
+++ dist/js/auth/login.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    const cookie = getCookie("token");
+    if (cookie) {
+        window.location.href = "../../index.html";
+    }
+
+    function getCookie(tokenType) {
+        let name = tokenType + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+})
